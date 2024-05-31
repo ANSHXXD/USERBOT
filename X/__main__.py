0edf1a44a941563ae7d437bbfcdb51d5d4d370b7
ANSHXXD <sreyanshsingh23@gmail.com>
2024-05-31 07:36:47 +0530

    Update __main__.py

--- X/__main__.py
+++ X/__main__.py
@@ -11,14 +11,14 @@ from X.helpers.misc import create_botlog, heroku
 BOT_VER = "3.0.0"
 CMD_HANDLER = ["." "?" "!" "*"]
 MSG_ON = """
-✧✧ **𝐃ɪᴄᴛᴀᴛᴏʀ 𝐔sᴇʀ𝐁ᴏᴛ 𝐈s 𝐀ʟɪᴠᴇ** ✧✧
+✧✧ **𝐀ηѕн 𝐔sᴇʀ𝐁ᴏᴛ 𝐈s 𝐀ʟɪᴠᴇ** ✧✧
 ▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭
 
-➥ **𝐃ɪᴄᴛᴀᴛᴏʀ 𝐕ᴇʀsɪᴏɴ 🥀 ** `{}`
+➥ **𝐀ηѕн 𝐕ᴇʀsɪᴏɴ 🥀 ** `{}`
 ➥ **𝐓ʏᴘᴇ** **.alive** **𝐓ᴏ 𝐂ʜᴇᴄᴋ 𝐀ʟɪᴠᴇ 𝐎ғ 「🇦 𝐍 𝐒 𝐇 」 𝐔sᴇʀ𝐁ᴏᴛ**
 ▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭
-➥ 𝐎ᴡɴᴇʀ :- @osmium_xd
-➥ 𝐆ʀᴘ :- @the_castless
+➥ 𝐎ᴡɴᴇʀ :- @ansh_xxd
+➥ 𝐆ʀᴘ :- @r2f_chats
 ➥ 𝐂ʜᴀɴ :- @ansh_xdz
 ▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭
 """
@@ -29,7 +29,7 @@ async def main():
     print("𝐋𝐎𝐆: 𝐅𝐨𝐮𝐧𝐝𝐞𝐝 𝐁𝐨𝐭 𝐭𝐨𝐤𝐞𝐧 𝐁𝐨𝐨𝐭𝐢𝐧𝐠..")
     for all_module in ALL_MODULES:
         importlib.import_module("X.modules" + all_module)
-        print(f"𝐃ɪᴄᴛᴀᴛᴏʀ 𝐔sᴇʀ𝐁ᴏᴛ 𝐒ᴜᴄᴄᴇssғᴜʟʟʏ 𝐈ᴍᴘᴏʀᴛᴇᴅ {all_module} ")
+        print(f"𝐀ηѕн 𝐔sᴇʀ𝐁ᴏᴛ 𝐒ᴜᴄᴄᴇssғᴜʟʟʏ 𝐈ᴍᴘᴏʀᴛᴇᴅ {all_module} ")
     for bot in bots:
         try:
             await bot.start()
@@ -39,7 +39,7 @@ async def main():
                 await bot.send_message(BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HANDLER))
             except BaseException:
                 pass
-            print(f"𝐘ᴏᴜʀ 𝐃ɪᴄᴛᴀᴛᴏʀ 𝐔sᴇʀ𝐁ᴏᴛ 𝐒ᴛᴀʀᴛᴇᴅ 𝐀s{ex.first_name} | {ex.id} ")
+            print(f"𝐘ᴏᴜʀ 𝐀ηѕн 𝐔sᴇʀ𝐁ᴏᴛ 𝐒ᴛᴀʀᴛᴇᴅ 𝐀s{ex.first_name} | {ex.id} ")
             ids.append(ex.id)
         except Exception as e:
             print(f"{e}")
